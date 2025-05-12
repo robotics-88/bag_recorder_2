982022778016649515ef5bb3d63d163269755527
Gus Meyer <gus@robotics88.com>
2025-05-12 13:05:54 -0700

    Clang formatting

--- include/bag_recorder_2/bag_recorder.hpp
+++ include/bag_recorder_2/bag_recorder.hpp
@@ -1,23 +1,23 @@
-#include <rclcpp/rclcpp.hpp>
 #include "bag_recorder_2/srv/record.hpp"
+#include <rclcpp/rclcpp.hpp>
 
-#include "rosbag2_transport/recorder.hpp"
 #include "rosbag2_transport/reader_writer_factory.hpp"
+#include "rosbag2_transport/recorder.hpp"
 
-class BagRecorder : public rclcpp::Node
-{
-public:
+class BagRecorder : public rclcpp::Node {
+  public:
     BagRecorder(rclcpp::executors::MultiThreadedExecutor &executor);
     ~BagRecorder() = default;
 
-private:
+  private:
     rclcpp::Service<bag_recorder_2::srv::Record>::SharedPtr record_service_;
     void trigger_recording(const std::shared_ptr<bag_recorder_2::srv::Record::Request> request,
-                                 std::shared_ptr<bag_recorder_2::srv::Record::Response> response);
+                           std::shared_ptr<bag_recorder_2::srv::Record::Response> response);
     void start_recording(std::string config_file, std::string data_directory);
     void stop_recording();
 
-    bool load_config(std::string config_name, std::vector<std::string>& topics, std::set<std::string> loaded = std::set<std::string>());
+    bool load_config(std::string config_name, std::vector<std::string> &topics,
+                     std::set<std::string> loaded = std::set<std::string>());
     std::string sanitize_topic(std::string topic);
 
     bool is_recording_;
@@ -25,5 +25,4 @@ private:
     std::thread recording_thread_;
 
     rclcpp::executors::MultiThreadedExecutor &executor_;
-
 };
